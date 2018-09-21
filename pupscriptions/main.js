4170602a8fbf1020e708672d2c8c90e1ab6aad55
Ruth Reyer <ruthmichaela@tutanota.com>
2018-09-21 16:27:58 -0400

    Added sidebar, instructions on front page input, and adjusted visual design.

--- pupscriptions/main.js
+++ pupscriptions/main.js
@@ -6,6 +6,7 @@ const dog = {
 	medications: [
 		{
 			name: "Rimadyl",
+			strength: 250,
 			dose:  1,
 			schedule: [
 				{
@@ -44,21 +45,34 @@ const dog = {
 
 
 const Pupscript = {
+	dogNameInput: null,
+	
 	view: function(vnode){
+		
+		var medsSidebar = []
+		for(let i = 0; i < dog.medications.length; i++){
+			medsSidebar.push(m("p", {class: "side-text"}, dog.medications[i].name + " " + dog.medications[i].strength + "mg"));
+		}
+		
 		
 		if(!dog.name){
 			return m("div", {class: "intro-div"}, [
 				m("h1", "Let's get started"),
 				m("input", {
-				class: "field",
-				type: "text",
-				placeholder: "Enter Dog's Name",
-				onkeypress: function(event) {
-					if(event.keyCode == 13){
-						dog.name = event.target.value;
-					}
-				},
-				})
+					class: "field",
+					type: "text",
+					placeholder: "Enter Dog's Name",
+					oninput: function(event) {
+						vnode.state.dogNameInput = event.target.value;
+					},
+					onkeypress: function(event) { //these can only manipulate data.
+						if(event.keyCode == 13){
+							dog.name = vnode.state.dogNameInput;
+						}
+					},
+				}),
+				
+				vnode.state.dogNameInput && vnode.state.dogNameInput.length > 2 ? m("p", "Press enter when finished.") : null
 				
 				]);
 				
@@ -70,60 +84,77 @@ const Pupscript = {
 					m(MedicationForm, {medication: dog.medications[i]})
 				);
 			}
-				
-			return m("div", {class: "main-container"}, [
-				m("h1", "Enter " + dog.name + "'s" + " medication information"),
-				
-				formElements,
-				
-				m("h2", dog.name + "'s" + " total medications: " + dog.medications.length),
-				
-				m("div", {class: "button-box"},
-				
-				m("button", {
-					onclick: function(event) {
-						dog.medications.push({
-							name:"Medication Name",
-							dose:0,
-							schedule:  [
-								{
-									am: false,
-									pm: false
-								},
-								{
-									am: false,
-									pm: false,
-								},
-								{
-									am: false,
-									pm: false
-								},
-								{
-									am: false,
-									pm: false,
-								},
-								{
-									am: false,
-									pm: false
-								},
-								{
-									am: false,
-									pm: false,
-								},
-								{
-									am: false,
-									pm: false
-								}
-							]
-						});
-					}
-				}, "+ " + "add another medication"),
-				
-				m("button", "Done"), //will go to route2
-				
-				//m("code", JSON.stringify(dog)) //gives up to date view of dog
-				)
-			])
+			
+			return [
+				//parent of all
+				m("div", {class: "top-container"}, [
+					
+					//main center
+					m("div", {class: "main-container"}, [
+						m("h1", "Enter " + dog.name + "'s" + " medication information"),
+						
+						formElements,
+						
+						m("h2", dog.name + "'s" + " total medications: " + dog.medications.length),
+						
+						m("div", {class: "button-box"},
+						
+						m("button", {
+							onclick: function(event) {
+								dog.medications.push({
+									name:"Medication Name",
+									strength: 0,
+									dose:0,
+									schedule:  [
+										{
+											am: false,
+											pm: false
+										},
+										{
+											am: false,
+											pm: false,
+										},
+										{
+											am: false,
+											pm: false
+										},
+										{
+											am: false,
+											pm: false,
+										},
+										{
+											am: false,
+											pm: false
+										},
+										{
+											am: false,
+											pm: false,
+										},
+										{
+											am: false,
+											pm: false
+										}
+									]
+								}, 
+								);
+							}
+						}, "+ " + "add another medication"),
+						
+						m("button", "Done"), //will go to route2
+					
+					//m("code", JSON.stringify(dog)) //gives up to date view of dog
+					)
+					]),
+					
+					//sidebar
+					m("span", {class: "side-container"}, [
+						m("h2", {class: "side-text"}, dog.name + "'s" + " medications"),
+						medsSidebar,
+						m("hr", {class: "side-hr"}),
+						m("p", {class: "side-text-total"}, "Total medications: " + dog.medications.length),
+					]),
+				])
+			]
 		}		
 	}
 };
@@ -150,6 +181,12 @@ const MedicationForm = {
 			m('input', {placeholder: "Name", class: "field", value: vnode.attrs.medication.name, 
 				oninput: function(event){
 					vnode.attrs.medication.name = event.target.value; //this is what makes the user input get stored as data
+				}
+			}),
+			m("h2", "Milligram:"),
+			m('input', {placeholder: 250, class: "field", value: vnode.attrs.medication.strength, 
+				oninput: function(event){
+					vnode.attrs.medication.strength = event.target.value; //this is what makes the user input get stored as data
 				}
 			}),
 			m("h2", "Number of pills per dose:"),
@@ -190,5 +227,16 @@ const ScheduleDay = {
 		]);
 	}	
 }
+
+const SideBar = {
+	view: function(vnode){
+		return m("div", {class: "side-bar"}, [
+					m("h2", dog.name + "'s" + " med box"),
+					m("h2", dog.name + "'s" + " total medications: " + dog.medications.length),
+						
+		])
+	}	
+}
+
 
 m.mount(root, Pupscript);
